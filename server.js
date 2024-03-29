83d909fed495d7f00627650805177ffc5a29b98f
mcotliar <m.cotliar+github@gmail.com>
2024-03-29 17:14:40 +0000

    Registration of New Users

--- server.js
+++ server.js
@@ -42,9 +42,41 @@ myDB(async client => {
     res.render('index', {
       title: 'Connected to Database',
       message: 'Please login', 
-      showLogin: true
+      showLogin: true,
+      showRegistration: true
     });
   });
+
+  app.route('/register')
+  .post((req, res, next) => {
+    myDataBase.findOne({ username: req.body.username }, (err, user) => {
+      if (err) {
+        next(err);
+      } else if (user) {
+        res.redirect('/');
+      } else {
+        myDataBase.insertOne({
+          username: req.body.username,
+          password: req.body.password
+        },
+          (err, doc) => {
+            if (err) {
+              res.redirect('/');
+            } else {
+              // The inserted document is held within
+              // the ops property of the doc
+              next(null, doc.ops[0]);
+            }
+          }
+        )
+      }
+    })
+  },
+    passport.authenticate('local', { failureRedirect: '/' }),
+    (req, res, next) => {
+      res.redirect('/profile');
+    }
+  );
 
   app.route('/login').post(passport.authenticate('local',{ failureRedirect: '/' } ), (req, res) => {
     // Change the response to render the Pug template
@@ -71,7 +103,7 @@ myDB(async client => {
       .type('text')
       .send('Not Found');
   });
-  
+
   passport.deserializeUser((id, done) => {
     myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
       done(null, doc);
